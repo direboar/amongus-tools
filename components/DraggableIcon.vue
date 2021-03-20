af1b2da75908add9bc42d92b0ef18d365d7fa78f
direboar <minokuba@example.com>
2021-03-20 18:32:33 +0900

    アイコン順序を微調整

--- components/DraggableIcon.vue
+++ components/DraggableIcon.vue
@@ -160,17 +160,20 @@ export default {
     dblclick() {
       if (this.icon.includes('clue') && this.status === '') {
         this.icon = `/icon/clue/${this.color}.png`
+        this.status = '/icon/status/gray.png'
+      } else if (this.icon.includes('clue') && this.status.includes('gray')) {
+        this.icon = `/icon/clue/${this.color}.png`
+        this.status = '/icon/status/sirokaku.png'
+      } else if (
+        this.icon.includes('clue') &&
+        this.status.includes('sirokaku')
+      ) {
+        this.icon = `/icon/clue/${this.color}.png`
         this.status = '/icon/status/impostor.png'
       } else if (
         this.icon.includes('clue') &&
         this.status.includes('impostor')
       ) {
-        this.icon = `/icon/clue/${this.color}.png`
-        this.status = '/icon/status/gray.png'
-      } else if (this.icon.includes('clue') && this.status.includes('gray')) {
-        this.icon = `/icon/clue/${this.color}.png`
-        this.status = '/icon/status/sirokaku.png'
-      } else if (this.status.includes('sirokaku')) {
         this.icon = `/icon/dead/${this.color}.png`
         this.status = ''
       } else if (this.icon.includes('dead')) {
